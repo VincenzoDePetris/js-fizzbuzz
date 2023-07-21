2cefd2feba9ca892a27f3ba295556b3857d304a8
VincenzoDePetris <vince02@live.it>
2023-07-21 14:59:25 +0200

    add: js fizz and buzz

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// -stampo i numero da 1 a 100
+// -trovo il modo di prendere i multipli di 3
+// -trovo il modo di prendere i multipli di 5
+// -li differenzio
+
+const maxNumber = 100;
+const minNumber = 1;
+
+for (i = minNumber; i <= maxNumber; i++) {
+  if (i % 3 == 0) {
+    console.log("buzz");
+  } else if (i % 5 == 0) {
+    console.log("fizz");
+  } else if (i % 5 == 0 && i % 3 == 0) {
+    console.log("fizzbuzz");
+  } else {
+    console.log(i);
+  }
+}
